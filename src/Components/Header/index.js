a48380f20b81012331df53dd9e0da05ac2bf7825
Jack Yeh <jack.ffy@gmail.com>
2018-03-09 12:08:08 +1300

    Set basic styles for header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import style from './index.css';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <div className="logo">Logo</div>
+        <div className={style.logo}>Logo</div>
         <NavBar />
-        <div className="menuIcon">
+        <div className={style.menuIcon}>
           <i className="fas fa-bars fa-2x" />
         </div>
       </header>
