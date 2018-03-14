3934b90352571282f995ddfd7138a22d5f85f12c
Jack Yeh <jack.ffy@gmail.com>
2018-03-15 12:35:56 +1300

    Fix merge issue

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Components/Header';
 import Intro from './Components/Intro';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import About from './Components/About';
 import Blog from './Components/Blog';
 import '@css/normalize.css';
 
-=======
 import Footer from './Components/Footer';
-=======
-import Team from './Components/Team';
->>>>>>> feature/team
-import './normalize.css';
->>>>>>> feature/footer
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Intro />
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <About />
-        <Blog />
-=======
-        <Footer />
->>>>>>> feature/footer
-=======
-        <Team />
->>>>>>> feature/team
       </div>
     );
   }
