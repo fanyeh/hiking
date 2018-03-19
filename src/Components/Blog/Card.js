05d6ec1232898a98131b8858f493ce6d5780284d
Jack Yeh <jack.ffy@gmail.com>
2018-03-19 14:38:47 +1300

    Update style names

--- src/Components/Blog/Card.js
+++ src/Components/Blog/Card.js
@@ -4,22 +4,22 @@ class Card extends Component {
   render() {
     const { imageSource } = this.props;
     return (
-      <div className={style.container}>
-        <div className={style.imageContainer}>
+      <div className={style.card}>
+        <div className={style.imageWrapper}>
           <div className={style.mask} />
           <img className={style.image} src={imageSource} alt="" />
         </div>
 
         <article className={style.content}>
-          <h3>Adventure Time</h3>
-          <time>02.11.2016</time>
-          <p>
+          <h3 className={style.title}>Adventure Time</h3>
+          <time className={style.time}>02.11.2016</time>
+          <p className={style.article}>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem harum aspernatur
             sapiente error, voluptas fuga, laudantium ullam magni fugit. Qui!
           </p>
           <div className={style.category}>
-            <span>travel</span>
-            <span>Blog</span>
+            <div>Travel</div>
+            <div>Blog</div>
           </div>
         </article>
       </div>
